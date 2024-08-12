d2fad48cac22a855c647e82fd5f98b0f73250f0b
Rishmo <100657272+Rishmo@users.noreply.github.com>
2024-08-12 14:27:17 +0530

    JS

--- practice_arr_method_8.js
+++ practice_arr_method_8.js
@@ -11,5 +11,5 @@ console.log("Average= ",avg);
 
 console.log("Question 2: ");
 let arr= [1,3,4,-2,-4];
-console.log("Number is: ",)
+console.log("Number is: ",arr);
 console.log(arr.map((no) => no + 5));
