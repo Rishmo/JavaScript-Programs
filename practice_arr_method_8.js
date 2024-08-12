1e3706a1c6e1c94cd1a263469c18af0c8937272b
Rishmo <100657272+Rishmo@users.noreply.github.com>
2024-08-12 15:42:40 +0530

    update

--- practice_arr_method_8.js
+++ practice_arr_method_8.js
@@ -13,3 +13,21 @@ console.log("Question 2: ");
 let arr= [1,3,4,-2,-4];
 console.log("Number is: ",arr);
 console.log(arr.map((no) => no + 5));
+
+console.log("Question 3: ");
+let str=["moon","ravi"];
+console.log("Strings are: ",str);
+console.log(str.map((strn) => strn.toUpperCase()));
+
+console.log("Question 4: ");
+let doubleAndReturnArgs = (ar, ...args) => [
+    ...ar,
+    ...args.map((v) => v*2),
+];
+
+console.log(doubleAndReturnArgs([1,2,3],4,5));
+console.log(doubleAndReturnArgs([1],2,3,4,5));
+
+console.log("Question 5: ");
+let mergeObjects = (obj1, obj2) =>({ ...obj1, ...obj2});
+mergeObjects({ a:1, b:2}, {c:3, d:4});
