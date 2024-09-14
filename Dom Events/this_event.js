027d5a7cea7f9b024b2f437e854f334e276b145b
Rishmo <100657272+Rishmo@users.noreply.github.com>
2024-09-14 20:59:04 +0530

    files added

---
+++ Dom Events/this_event.js
@@ -0,0 +1,19 @@
+// let btn =  document.querySelector("button");
+let btns = document.querySelector("button");
+let h1 =  document.querySelector("h1");
+let p =  document.querySelector("p");
+
+// function changeColor() {
+//     console.dir(this.innerText());
+//     this.style.backgroundColor = "blue";
+// }
+
+// btn.addEventListener("click", changeColor);
+// h1.addEventListener("click", changeColor);
+// p.addEventListener("click", changeColor);
+
+btns.addEventListener("click", function() {
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
+});
+
