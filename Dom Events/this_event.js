9f52b1556056a12b12b7dc71ee6319d97d16bdc7
Rishmo <100657272+Rishmo@users.noreply.github.com>
2024-09-15 20:10:49 +0530

    file added

--- Dom Events/this_event.js
+++ Dom Events/this_event.js
@@ -1,16 +1,16 @@
-// let btn =  document.querySelector("button");
+let btn =  document.querySelector("button");
 let btns = document.querySelector("button");
 let h1 =  document.querySelector("h1");
 let p =  document.querySelector("p");
 
-// function changeColor() {
-//     console.dir(this.innerText());
-//     this.style.backgroundColor = "blue";
-// }
+function changeColor() {
+    console.dir(this.innerText());
+    this.style.backgroundColor = "blue";
+}
 
-// btn.addEventListener("click", changeColor);
-// h1.addEventListener("click", changeColor);
-// p.addEventListener("click", changeColor);
+btn.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor);
+p.addEventListener("click", changeColor);
 
 btns.addEventListener("click", function() {
     console.dir(this.innerText);
